210cccdbf3cdc688679f2b9728cc77ce643a7acc
sakib-xrz <sakibxrz21@gmail.com>
2025-09-08 15:23:43 +0600

    feat(cors): add new origin for CORS to support medimart-store frontend

--- dist/app.js
+++ dist/app.js
@@ -18,7 +18,11 @@ app.use(express_1.default.urlencoded({ extended: true, limit: '50mb' }));
 app.use(body_parser_1.default.urlencoded({ extended: true, limit: '50mb' }));
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:3000', 'https://medimart-self.vercel.app'],
+    origin: [
+        'http://localhost:3000',
+        'https://medimart-self.vercel.app',
+        'https://medimart-store.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept',
     credentials: true,
